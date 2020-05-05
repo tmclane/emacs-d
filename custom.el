7797de510b0118efaf4fd2dea3fb9f7a60bdc8b8
Travis McLane <tmclane@gmail.com>
2020-05-04 19:28:27 -0500

    initial commit

---
+++ custom.el
@@ -0,0 +1,65 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(auto-revert-verbose nil)
+ '(auto-save-list-file-prefix meta-saves)
+ '(backup-by-copying-when-linked t)
+ '(backup-directory-alist (quote ((".*" . "~/.emacs-meta/backups/"))))
+ '(bookmark-default-file meta-bookmarks)
+ '(custom-file (expand-file-name "custom.el" user-emacs-directory))
+ '(delete-old-versions t)
+ '(display-time-day-and-date nil)
+ '(display-time-default-load-average nil)
+ '(display-time-format "%l:%M%p")
+ '(echo-keystrokes 0.1)
+ '(fill-column 75)
+ '(git-gutter:lighter " GG")
+ '(global-auto-revert-non-file-buffers t)
+ '(history-delete-duplicates t)
+ '(indent-tabs-mode nil)
+ '(indicate-empty-lines t)
+ '(inhibit-startup-echo-area-message (user-login-name))
+ '(inhibit-startup-screen t)
+ '(kept-new-versions 2)
+ '(kept-old-versions 2)
+ '(mouse-wheel-follow-mouse (quote t))
+ '(mouse-wheel-progressive-speed nil)
+ '(mouse-wheel-scroll-amount (quote (1 ((shift) . 1))))
+ '(mouse-yank-at-point t)
+ '(next-line-add-newlines t)
+ '(package-selected-packages
+   (quote
+    (helm-c-yasnippet yasnippet-snippets yaml-mode svelte-mode paradox org-wiki org-sticky-header org-bullets markdown-mode go-snippets go-guru go-mode go-autocomplete go-imports auto-complete spaceline-all-the-icons no-littering exec-path-from-shell auto-compile quelpa-use-package org-plus-contrib diminish)))
+ '(recentf-max-menu-items 15)
+ '(recentf-max-saved-items 100)
+ '(recentf-save-file meta-recent)
+ '(require-final-newline t)
+ '(save-place-file meta-saveplace)
+ '(savehist-additional-variables (quote (kill-ring search-ring regexp-search-ring)))
+ '(savehist-autosave-interval 300)
+ '(savehist-file meta-savehist)
+ '(scroll-step 1)
+ '(select-enable-clipboard t)
+ '(shift-select-mode nil)
+ '(tab-width 4)
+ '(truncate-partial-width-windows 50)
+ '(undo-tree-visualizer-diff t)
+ '(undo-tree-visualizer-timestamps t)
+ '(url-cache-directory (concat temporary-file-directory "url/cache"))
+ '(url-cookie-file (concat meta-url "/cookies"))
+ '(use-package-verbose t)
+ '(version-control t)
+ '(visible-bell t)
+ '(whitespace-line-column 80)
+ '(whitespace-style
+   (quote
+    (face trailing space-before-tab indentation space-after-tab)))
+ '(x-select-request-type (quote (UTF8_STRING COMPOUND_TEXT TEXT STRING))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
---
+++ init.el
@@ -0,0 +1,84 @@
+;;; init.el --- Emacs configuration file
+;;
+;; Converted to literate format
+;; Author: John Eastman
+;; Date: 2013-10-24
+;;
+;;; Commentary:
+;;
+;; Bootstrap initialization file.  Just enough configuration
+;; to initialize the packaging system and ensure org is loaded
+;; so that the rest of the configuration can be loaded.
+;;
+;;; Code:
+;;
+
+;; Bling on Software tip about Garbage Collection
+;; http://bling.github.io/blog/2016/01/18/why-are-you-changing-gc-cons-threshold/
+;; Original source appears to be:
+;; https://www.reddit.com/r/emacs/comments/3kqt6e/2_easy_little_known_steps_to_speed_up_emacs_start/
+(let ((gc-cons-threshold most-positive-fixnum))
+
+  ;; Warn about using older versions of Emacs
+  ;; Nothing specific here, but this configuration is developed and maintained on
+  ;; version specified here.
+  (when (version< emacs-version "25")
+    (warn "This configuration expects Emacs version 25 or greater - you are running %s!"
+          emacs-version))
+
+  ;; require the package manager
+  (require 'package)
+
+  ;; Set some startup configuration values
+  (setq-default
+   load-prefer-newer t            ; load prefers the newest version of a file
+   package-enable-at-startup nil) ; Prevent packages from being activated after reading init
+
+  ;; initialize the ELPA repositories where packages are fetched
+  (setq package-archives '(("org"   . "http://orgmode.org/elpa/")
+                           ("melpa" . "https://melpa.org/packages/")
+                           ("gnu"   . "https://elpa.gnu.org/packages/")))
+
+  (package-initialize)
+
+  ;; Utilize use-package
+  (unless (package-installed-p 'use-package)
+    (package-refresh-contents)
+    (package-install 'use-package))
+
+  ;; Utilize use-package
+  (unless (package-installed-p 'quelpa)
+    (package-refresh-contents)
+    (package-install 'quelpa)
+    (package-install 'quelpa-use-package))
+
+  (eval-when-compile
+    (require 'use-package)
+    (require 'quelpa-use-package))
+
+  (unless (package-installed-p 'diminish)
+    (package-refresh-contents)
+    (package-install 'diminish)
+    (require 'diminish))
+
+  (require 'bind-key)
+
+  (custom-set-variables
+   '(use-package-verbose t))
+
+  ;; load org and make sure we have the right version
+  (use-package org
+    :ensure org-plus-contrib
+    :pin org)
+
+  ;; load the literate init file
+  (org-babel-load-file (expand-file-name "init-ext.org" user-emacs-directory))
+
+  ;; trigger garbage collection to clean up our deferred mess
+  (garbage-collect))
+
+(put 'upcase-region 'disabled nil)
+
+(provide 'init)
+;;; init.el ends here
+
