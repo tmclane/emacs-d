9688b1437098438afc4b11f0d4349ba8a5984515
Travis McLane <tmclane@gmail.com>
2022-08-30 21:06:00 -0500

    tdm-go cleanup

--- modules/tdm-go.el
+++ modules/tdm-go.el
@@ -27,93 +27,35 @@
 ;;; Code:
 (require 'straight)
 (require 'jme-common)
-(straight-use-package 'use-package)
-(use-package go-imports
-  :straight t
-  :demand t
-  :ensure t)
-
-(use-package go-autocomplete
-  :straight t
-  :demand t
-  :ensure t
-  :config
-  (ac-config-default))
-
-(use-package go-mode
-  :straight t
-  :demand t
-  :ensure t
-  :config
-  (setq gofmt-command "goimports")
-  (add-hook 'before-save-hook #'gofmt-before-save))
-;;  (add-hook 'completion-at-point-functions 'go-complete-at-point))
-
-;; (use-package go-guru
-;;   :demand t
-;;   :ensure t)
-
-(use-package go-snippets
-  :straight t
-  :demand t
-  :ensure t)
-
-(straight-use-package 'rustic)
-
-(defun jme-rust--enable ()
-  "Enable configuration."
-  (custom-set-variables
-   '(rustic-lsp-client 'eglot))
-  (add-hook 'rustic-mode-hook #'jme-common-auto-save-on-compilation))
-
-(defun jme-rust--disable ()
-  "Disable configuration."
-  (remove-hook 'rustic-mode-hook #'jme-common-auto-save-on-compilation))
-
-(jme-common-defconfiguration jme-rust "Configuration for Rust programming")
-
-(provide 'jme-rust)
-;;; jme-rust.el ends here
-;;; jme-rust.el --- Configuration for Rust programming  -*- lexical-binding: t; -*-
-
-;; Copyright (C) 2022  John Eastman
-
-;; Author: John Eastman <jeastman@miggle.lan>
-;; Keywords: convenience, languages
-
-;; This file is not part of GNU Emacs.
-
-;; This program is free software; you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation, either version 3 of the License, or
-;; (at your option) any later version.
-
-;; This program is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
-
-;; You should have received a copy of the GNU General Public License
-;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
-;;; Commentary:
-
-;; Provides configuration of `go-mode' package for use with Go programming.
-
-;;; Code:
-(require 'straight)
-(require 'jme-common)
 (straight-use-package 'go-mode)
 (straight-use-package 'go-imports)
-(straight-use-package 'go-autocomplete)
+(setq gofmt-command "goimports")
+
+                    ;(straight-use-package 'go-autocomplete)
+                    ;(straight-use-package 'eglot)
+(straight-use-package 'project)
+(require 'project)
+
+(defun project-find-go-module (dir)
+  (when-let ((root (locate-dominating-file dir "go.mod")))
+    (cons 'go-module root)))
+
+(cl-defmethod project-root ((project (head go-module)))
+  (cdr project))
+;  (add-hook 'go-mode-hook 'eglot-ensure)
 
 (defun tdm-go--enable ()
   "Enable configuration."
-  (add-hook 'before-save-hook #'gofmt-before-save))
+
+  (add-hook 'before-save-hook 'gofmt-before-save)
+  (add-hook 'project-find-functions 'project-find-go-module))
+
+;  (remove-hook 'go-mode-hook 'eglot-ensure)
 
 (defun tdm-go--disable ()
   "Disable configuration."
-  (remove-hook 'before-save-hook 'gofmt-before-save t))
+  (remove-hook 'before-save-hook 'gofmt-before-save t)
+  (remove-hook 'project-find-functions 'project-find-go-module))
 
 (jme-common-defconfiguration tdm-go "Configuration for Go programming")
 
