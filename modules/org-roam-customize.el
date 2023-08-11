5c6ec20bc4d7fe47b48eacc4a225dd317de07d5b
Travis McLane <travis.d.mclane.ctr@army.mil>
2023-08-11 16:25:12 -0500

    add org-roam functions

--- modules/org-roam-customize.el
+++ modules/org-roam-customize.el
@@ -1,13 +1,12 @@
 (require 'straight)
 
-;(org-roam-directory "~/Documents/org-roam")
-
 (use-package org-roam
   :ensure t
  :straight t
   :init
   (setq org-roam-v2-ack t)
   :custom
+  (org-roam-directory "~/Documents/org-roam")
   (org-roam-completion-everywhere t)
   :bind (("C-c n l" . org-roam-buffer-toggle)
          ("C-c n f" . org-roam-node-find)
@@ -22,5 +21,22 @@
   :config
   (require 'org-roam-dailies) ;; Ensure the keymap is available
   (org-roam-db-autosync-mode))
+
+(defun my/org-roam-filter-by-tag (tag-name)
+  (lambda (node)
+    (member tag-name (org-roam-node-tags node))))
+
+(defun my/org-roam-list-notes-by-tag (tag-name)
+  (mapcar #'org-roam-node-file
+          (seq-filter
+           (my/org-roam-filter-by-tag tag-name)
+           (org-roam-node-list))))
+
+(defun my/org-roam-refresh-agenda-list ()
+  (interactive)
+  (setq org-agenda-files (my/org-roam-list-notes-by-tag "Project")))
+
+;; Build the agenda list the first time for the session
+(my/org-roam-refresh-agenda-list)
 
 (provide 'org-roam-customize)
